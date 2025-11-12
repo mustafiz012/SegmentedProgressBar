8a28974b561e6930095e07de574d509d6d49bdcd
mustafiz012 <mustafiz012@outlook.com>
2025-11-13 01:08:51 +0600

    Fix Handler initialization and update conditions
    - Explicitly initialize Handler with the main looper.
    - Simplify a conditional check for segment animation state.
    - Add parentheses to clarify a condition in the animation `run` method.

--- segmentedprogressbar/src/main/java/pt/tornelas/segmentedprogressbar/SegmentedProgressBar.kt
+++ segmentedprogressbar/src/main/java/pt/tornelas/segmentedprogressbar/SegmentedProgressBar.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.os.Handler
+import android.os.Looper
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
@@ -55,7 +56,7 @@ class SegmentedProgressBar : View, Runnable, ViewPager.OnPageChangeListener, Vie
     private val selectedSegmentIndex: Int
         get() = segments.indexOf(this.selectedSegment)
 
-    private val animationHandler = Handler()
+    private val animationHandler = Handler(Looper.getMainLooper())
     private val animationUpdateTime: Long
         get() = timePerSegmentMs / 100
 
@@ -251,7 +252,7 @@ class SegmentedProgressBar : View, Runnable, ViewPager.OnPageChangeListener, Vie
             } else if (offset < 0) {
                 if (index > nextSegmentIndex - 1) segment.animationState =
                     Segment.AnimationState.IDLE
-            } else if (offset == 0) {
+            } else {
                 if (index == nextSegmentIndex) segment.animationState = Segment.AnimationState.IDLE
             }
         }
@@ -279,7 +280,7 @@ class SegmentedProgressBar : View, Runnable, ViewPager.OnPageChangeListener, Vie
     }
 
     override fun run() {
-        if (this.selectedSegment?.progress() ?: 0 >= 100) {
+        if ((this.selectedSegment?.progress() ?: 0) >= 100) {
             loadSegment(offset = 1, userAction = false)
         } else {
             this.invalidate()
